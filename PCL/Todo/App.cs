8b61a1f1c8d00921e80a803b4658e918f3ef2a4c
Kevin Wan <kevister@hotmail.com>
2016-10-24 14:01:54 -0700

    Added HomePage + MD; but MD's namespace not working

--- PCL/Todo/App.cs
+++ PCL/Todo/App.cs
@@ -14,7 +14,7 @@ namespace Todo
 			Resources.Add ("primaryGreen", Color.FromHex("91CA47"));
 			Resources.Add ("primaryDarkGreen", Color.FromHex ("6FA22E"));
 
-			var nav = new NavigationPage (new TodoItemListX ());
+			var nav = new NavigationPage (new HomePage ());
 			nav.BarBackgroundColor = (Color)App.Current.Resources["primaryGreen"];
 			nav.BarTextColor = Color.White;
 
@@ -38,23 +38,28 @@ namespace Todo
 
 			// always re-set when the app starts
 			// users expect this (usually)
-//			Properties ["ResumeAtTodoId"] = "";
-			if (Properties.ContainsKey ("ResumeAtTodoId")) {
-				var rati = Properties ["ResumeAtTodoId"].ToString();
-				Debug.WriteLine ("   rati="+rati);
-				if (!String.IsNullOrEmpty (rati)) {
-					Debug.WriteLine ("   rati = " + rati);
-					ResumeAtTodoId = int.Parse (rati);
+			//			Properties ["ResumeAtTodoId"] = "";
+			if (Properties.ContainsKey("ResumeAtTodoId"))
+			{
+				var rati = Properties["ResumeAtTodoId"].ToString();
+				Debug.WriteLine("   rati=" + rati);
+				if (!String.IsNullOrEmpty(rati))
+				{
+					Debug.WriteLine("   rati = " + rati);
+					ResumeAtTodoId = int.Parse(rati);
 
-					if (ResumeAtTodoId >= 0) {
-						var todoPage = new TodoItemPageX ();
-						todoPage.BindingContext = Database.GetItem (ResumeAtTodoId);
+					if (ResumeAtTodoId >= 0)
+					{
+						var todoPage = new TodoItemPageX();
+						todoPage.BindingContext = Database.GetItem(ResumeAtTodoId);
 
-						MainPage.Navigation.PushAsync (
-							todoPage,
-							false); // no animation
+						MainPage.Navigation.PushAsync(
+							new HomePage(), false); // no animation
 					}
 				}
+			}
+			else {
+				MainPage.Navigation.PushAsync(new HomePage(), false);
 			}
 		}
 		protected override void OnSleep()
---
+++ PCL/Todo/Views/MasterPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Todo
+{
+	public partial class MasterPage : ContentPage
+	{
+		public ListView ListView
+		{ get { return listView; } }
+
+		public MasterPage()
+		{
+			InitializeComponent();
+
+			var masterPageItems = new List<MasterPageItem>();
+			masterPageItems.Add(new MasterPageItem
+			{
+				Title = "Contacts",
+				//IconSource = "contacts.png",
+				TargetType = typeof(TodoItemListX)
+			});
+
+			listView.ItemsSource = masterPageItems;
+		}
+	}
+}
