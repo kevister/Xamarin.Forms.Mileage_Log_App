1c8e014312568e75aaa6aae1d6570e74a6a0d4e9
Kevin Wan <kevister@hotmail.com>
2016-12-08 11:48:21 -0800

    Working Mail Controller

--- PCL/Todo.iOS/Email_iOS.cs
+++ PCL/Todo.iOS/Email_iOS.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using Xamarin.Forms;
 using Todo;
+using MessageUI;
+using UIKit;
 
 [assembly: Dependency(typeof(Email_iOS))]
 
@@ -8,12 +10,32 @@ namespace Todo
 {
 	public class Email_iOS : IEmail
 	{
+		MFMailComposeViewController mailController;
+
 		public Email_iOS()
 		{
 		}
 		public void sendEmail()
 		{
 			Device.OpenUri(new Uri("mailto:ryan.hatfield@test.com"));
+
+			if (MFMailComposeViewController.CanSendMail)
+			{
+				mailController = new MFMailComposeViewController();
+
+				// do mail operations here
+				//mailController.SetToRecipients(new string[] { "john@doe.com" });
+				mailController.SetSubject("mail test");
+				mailController.SetMessageBody("this is a test", false);
+
+				mailController.Finished += (object s, MFComposeResultEventArgs args) =>
+				{
+					Console.WriteLine(args.Result.ToString());
+					args.Controller.DismissViewController(true, null);
+				};
+
+				UIApplication.SharedApplication.KeyWindow.RootViewController.PresentViewController(mailController, true, null);
+			}
 		}
 	}
 }
---
+++ PCL/Todo.iOS/TodoItem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using SQLite;
+
+namespace Todo
+{
+	public class TodoItem
+	{
+		public TodoItem ()
+		{
+		}
+
+		[PrimaryKey, AutoIncrement]
+		public int ID { get; set; }
+		public string SO { get; set; }
+		public string EO { get; set; }
+		public string Comments { get; set; }
+		public string Label { get; set;}
+		public DateTime TimeStamp { get; set; }
+
+	}
+}
+
